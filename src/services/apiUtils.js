4547e3ede94eecccf334d3d88a8eba06625722f0
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-05-17 09:30:40 -0700

    PUT route working

--- src/services/apiUtils.js
+++ src/services/apiUtils.js
@@ -27,6 +27,6 @@ export const makePutOrPostCall = async (url, json, type) => {
 //POST {"name":"HELLLLLOOOOOOOOO","species_id":4,"role":"sidekick","unique_power":"none","movie":"No Movie","movie_year":1951,"hand_drawn":true,"image":"https:blah.jpeg","gif":"https:blah.gif"}
 
 //PUT  takes id at end of string 
-//{name: 'OOOOOOOOOOOOOOOOOOO', owner_id: 1, species_id: 4, role: 'villan', unique_power: 'none', movie: 'MOVIE NAME', movie_year: 2020, hand_drawn: false, image: 'none', gif: 'none here either'}
+//{"name":"OOOOOOOOOOOOOOOOOOO", "owner_id":1, "species_id":4, "role":"villan", "unique_power":"none", "movie":"MOVIE NAME", "movie_year":2020, "hand_drawn":false, "image":"none", "gif": "none here either"}
 
-//delete by char id # at end of url
+//delete by char name at end of url
